d6fa162505653a9d8c0e02885f9d3c9b78349531
lucas <lucasgauvain@gmail.com>
2020-09-19 13:46:16 +0200

    ajout de la division

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print( "Veuillir saisir 2 nombres : " );
+        System.out.println( "Veuillir saisir 2 nombres : " );
         int facteur1 = Integer.parseInt(scanner.nextLine());
         int facteur2 = Integer.parseInt(scanner.nextLine());
         int produit = facteur1*facteur2;
@@ -16,6 +16,11 @@ public class Main {
         int modulo2 = Integer.parseInt(scanner.nextLine());
         int modulo3 = modulo1%modulo2;
         System.out.println("Le reste de vos 2 nombres est : " + modulo3);
+        System.out.println( "Veuillir saisir 2 nombres : " );
+        int diviseur1 = Integer.parseInt(scanner.nextLine());
+        int divisieur2 = Integer.parseInt(scanner.nextLine());
+        int division = diviseur1/divisieur2;
+        System.out.println("La division de vos 2 nombres est : " + division);
 
     }
 }
