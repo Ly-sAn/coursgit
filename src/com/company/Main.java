3ec1b7237a13120002f8c7b0dad0cabd41179e0e
lucas <lucasgauvain@gmail.com>
2020-09-19 12:26:27 +0200

    Ajout de l'âge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,10 @@ public class Main {
         System.out.print( "Veuillez saisir votre nom : " );
         String nomUtilisateur = scanner.nextLine();
         System.out.println("Bienvenue " + nomUtilisateur) ;
+        System.out.println("");
+        System.out.print( "Veuillez saisir votre age : " );
+        String age = scanner.nextLine();
+        int ageToInt = Integer.parseInt(age);
+        System.out.println("Vous avez " + ageToInt) ;
     }
 }
