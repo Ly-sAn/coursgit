cbfbffbcfe7faf2b866ab80ea936ccc1a66232fc
lucas <lucasgauvain@gmail.com>
2020-09-19 13:39:12 +0200

    Ajout de la multiplication

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-       
+        Scanner scanner = new Scanner(System.in);
+        System.out.print( "Veuillir saisir 2 nombres : " );
+        int facteur1 = Integer.parseInt(scanner.nextLine());
+        int facteur2 = Integer.parseInt(scanner.nextLine());
+        int produit = facteur1*facteur2;
+        System.out.println("Le produit de vos 2 facteurs est : " + produit);
+
     }
 }
