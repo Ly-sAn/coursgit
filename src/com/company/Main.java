63094437189bf25292027fcfee42ebe08d964052
lucas <lucasgauvain@gmail.com>
2020-09-19 13:24:07 +0200

    suppression du code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print( "Veuillez saisir votre nom : " );
-        String nomUtilisateur = scanner.nextLine();
-        System.out.println("Bienvenue " + nomUtilisateur) ;
-        System.out.println("");
-        System.out.print( "Veuillez saisir votre age : " );
-        String age = scanner.nextLine();
-        int ageToInt = Integer.parseInt(age);
-        System.out.println("Vous avez " + ageToInt) ;
-        System.out.println("HOLA");
+       
     }
 }
